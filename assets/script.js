d9e5f6a8c21214149c4a00068c12c909a9198769
Sammy Kroner <sammyk118@gmail.com>
2021-01-05 13:57:51 -0800

    working example api request

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,14 @@
 // x46hSJCLqaGAFDps6LRQpcLTBZRXAtdd
 
 //example call
-queryurl="https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=yourkey"
+queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=x46hSJCLqaGAFDps6LRQpcLTBZRXAtdd"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+  
+    .then(function (response) {
+        var results = response;
+        console.log(results);
+})
